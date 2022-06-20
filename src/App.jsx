54274c4515b41e4c7fc55f74396a81a07515e67e
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-06-20 15:57:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import './app.scss';
-import Home from './pages/Home/Home';
+// import Home from './pages/Home/Home';
 import Register from './pages/register/Register';
 // import Login from './pages/login/Login';
 
-import Watch from './pages/watch/Watch';
+// import Watch from './pages/watch/Watch';
 function App() {
   return (
    <Register/>
