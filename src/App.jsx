474b15c29300f8317ec617a1909a6a6fe46a0dd0
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-08-28 10:56:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 
 import './app.scss';
-// import Home from './pages/Home/Home';
+ import Home from './pages/Home/Home';
 import Register from './pages/register/Register';
-// import Login from './pages/login/Login';
-
-// import Watch from './pages/watch/Watch';
+ import Login from './pages/login/Login';
+ import Watch from './pages/watch/Watch';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
-  return (
-   <Register/>
-  );
+ return(    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/watch" element={<Watch/>}/>
+    
+     
+    
+    </Routes>
+  </BrowserRouter>)
 }
 
 export default App;
