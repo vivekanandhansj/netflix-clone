ca0d356f8a757affe6df18599d2610ab8eb6b0b2
vivekanandhansj <97933338+vivekanandhansj@users.noreply.github.com>
2022-08-28 11:01:41 +0100

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
            <h1>Sign In</h1>
            <input type="email" placeholder="Email or phone number"/>
            <input type="password" placeholder="Password"/>
-        <button className="loginButton">Sign In</button>
+        <button className="loginButton"  onClick={handleClick}>Sign In</button>
         <span>New to Netflix? <b>Sign up now</b> </span>
         <small>
         This page is protected by Google reCAPTCHA to ensure you're not a bot. <b>Learn more.</b>
